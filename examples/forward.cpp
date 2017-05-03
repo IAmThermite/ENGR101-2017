9a184c9bc6cc9b6f34461b1e94d21c7f8ab48e23
LuciusDev <luke.honiss@gmail.com>
2017-05-04 11:49:10 +1200

    Update forward.cpp

--- examples/forward.cpp
+++ examples/forward.cpp
@@ -4,9 +4,9 @@
 
 int main() {
     init();
-	sleep1(0, 1000);
-    set_motor(1, 254);
-    set_motor(2, -254);
+    sleep1(0, 1000);
+    set_motor(1, 128);
+    set_motor(2, -128);
     sleep1(1, 0);
     return 0;
 }
