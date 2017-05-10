1ea915f99d217f6d19dc7c4d5aeb2b86a08fe172
xavierbroadhead <xavier@Xaviers-MacBook-Air.local>
2017-05-11 10:45:05 +1200

    read_analog

--- src/controller.cpp
+++ src/controller.cpp
@@ -11,6 +11,7 @@ extern "C" int init(int d_lev);
 extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
+extern "C" int read_analog(int ch_adc); 
 
 int main(){
 	printf("My process ID : %d\n", getpid()); //for if we need to stop the process
